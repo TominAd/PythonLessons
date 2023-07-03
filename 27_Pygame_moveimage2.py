de2be9ded58f22c6327eef0601e8be7e84cb84fa
Tomin <denis.goroshko1990@gmail.com>
2023-07-03 12:57:02 +0300

    27_Pygame
    27_Pygame

--- 27_Pygame_moveimage2.py
+++ 27_Pygame_moveimage2.py
@@ -2,6 +2,7 @@ import pygame
 
 max_x = 800
 max_y = 600
+IMG_SIZE = 200
 game_over = False
 bg_color = (0, 10, 0)
 
@@ -12,25 +13,62 @@ pygame.display.set_caption("My First PyGame !:)")  # Наеменования о
 print(pygame.image.get_extended())  # выводит тру или фолс т.е. может прочитать картинку или нет. так как только в *.png
 
 myimage = pygame.image.load("16975.png").convert()
-myimage = pygame.transform.scale(myimage, (250, 250))  # Если надо уменьшить картинку любых размеров
+myimage = pygame.transform.scale(myimage, (IMG_SIZE, IMG_SIZE))  # Если надо уменьшить картинку любых размеров
 x = 500
 y = 100
+
+move_rigth = False
+move_left = False
+move_up = False
+move_down = False
+
 # ================main Game Loop
 while game_over == False:
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
+
             if event.key == pygame.K_ESCAPE:
                 game_over = True
             if event.key == pygame.K_LEFT:
-                x -= 20
+                move_left = True
             if event.key == pygame.K_RIGHT:
-                x += 20
+                move_rigth = True
             if event.key == pygame.K_UP:
-                y -= 20
+                move_up = True
             if event.key == pygame.K_DOWN:
-                y += 20
+                move_down = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT:
+                move_left = False
+            if event.key == pygame.K_RIGHT:
+                move_rigth = False
+            if event.key == pygame.K_UP:
+                move_up = False
+            if event.key == pygame.K_DOWN:
+                move_down = False
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             x, y = event.pos
+
+    if move_left == True:
+        x -= 1
+        if x < 0:
+            x = 0
+    if move_rigth == True:
+        x += 1
+        if x > max_x - IMG_SIZE:
+            x = max_x - IMG_SIZE
+    if move_up == True:
+        y -= 1
+        if y < 0:
+            Y = 0
+    if move_down == True:
+        y += 1
+        if y > max_y - IMG_SIZE:
+            y= max_y - IMG_SIZE
+
+
 
     screen.fill(bg_color)
     screen.blit(myimage, (x, y))

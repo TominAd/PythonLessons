f10af0f40cd8fc1dd220bed7a021d9b16bd45976
Tomin <denis.goroshko1990@gmail.com>
2023-07-04 10:20:34 +0300

    28_SnowFall
    28_SnowFall

--- 28_SnowFall.py
+++ 28_SnowFall.py
@@ -4,12 +4,12 @@ import sys
 import time
 
 MAX_X = 1920
-MAX_Y = 1082
+MAX_Y = 1080
 MAX_SNOW = 100
-SNOW_SIZE = 64
+SNOW_SIZE =64
 
 class Snow():
-    def __int__(self, x, y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
         self.speed = random.randint(1, 3)
@@ -17,35 +17,39 @@ class Snow():
         self.image_filename = "snow" + str(self.img_num) + ".png"
         self.image = pygame.image.load(self.image_filename).convert_alpha()
         self.image = pygame.transform.scale(self.image, (SNOW_SIZE, SNOW_SIZE))
+
     def move_snow(self):
         self.y = self.y + self.speed
         if self.y > MAX_Y:
             self.y = (0 - SNOW_SIZE)
 
         i = random.randint(1, 3)
-        if i == 1:  #move Right
+        if i == 1:
             self.x += 1
             if self.x > MAX_X:
                 self.x = (0 - SNOW_SIZE)
-
-        elif i == 2:   #Move Left
+        elif i == 2:
             self.x -= 1
             if self.x < (0 - SNOW_SIZE):
                 self.x = MAX_X
 
     def draw_snow(self):
         screen.blit(self.image, (self.x, self.y))
+
+
 def initilize_snow(max_snow, snowfall):
     for i in range(0, max_snow):
         xx = random.randint(0, MAX_X)
         yy = random.randint(0, MAX_Y)
         snowfall.append(Snow(xx, yy))
+
 def check_for_exit():
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
             sys.exit()
 
-# ==========MAIN+++++++++++++++
+#===========Main==============
+
 pygame.init()
 screen = pygame.display.set_mode((MAX_X, MAX_Y), pygame.FULLSCREEN)
 bg_color = (0, 0, 0)
@@ -59,8 +63,5 @@ while True:
     for i in snowfall:
         i.move_snow()
         i.draw_snow()
-
-    time.sleep(0.002)
+    time.sleep(0.020)
     pygame.display.flip()
-
-

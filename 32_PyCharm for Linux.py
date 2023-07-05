59a9413cd5a3309f22d339e3d6ff6a028fcd06f0
Tomin <denis.goroshko1990@gmail.com>
2023-07-05 09:58:54 +0300

    32_PyCharm for Linux
    32_PyCharm for Linux

---
+++ 32_PyCharm for Linux.py
@@ -0,0 +1,9 @@
+
+
+print("cd Desctop/")
+print("wget https://вставляем ссылку")
+print("ls -l") #чтоб посмотреть имя файла и что в папке
+print("tar -xvf pycha....") #разорхивировать
+print("cd pycha..... ")
+print("cd bin")
+print("./pycharm.sh")

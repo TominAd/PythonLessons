d6aeea1ce7786ddd85294a2ff4fcc66bfc2dfaa1
Tomin <denis.goroshko1990@gmail.com>
2023-07-05 16:52:38 +0300

    34_Django_install
    34_Django_install

---
+++ django/MyFirstDjango/MyFirstDjango/34_Django_install.py
@@ -0,0 +1,6 @@
+#pip install django  Установка джанго
+#django-admin --version  проверка версии
+#django-admin startproject MyFirstDjango создание проекта MyFirstDjango - имя меняем на любое другое
+# cd .\MyFirstDjango\  переходим в каталог который создал джанго
+#python .\manage.py migrate создаем базу данных в каталоге
+#python .\manage.py runserver  запускаем сервер
